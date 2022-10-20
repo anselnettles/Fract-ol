4cb81008c29371cd37f0cb5e608b4a6d2b6f5738
anselnettles <aviholai@student.hive.fi>
2022-10-20 18:40:59 +0300

    New file for MLX graphical controls.

---
+++ mlx_control.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_control.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 18:21:54 by aviholai          #+#    #+#             */
+/*   Updated: 2022/10/20 18:37:02 by aviholai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "the_uncontrolled_element_of_life.h"
+
+int	keypress(int key, void *param)
+{
+	t_fract	*f;
+
+	f = (t_fract *)param;
+	if (key)
+	{
+		if (key == TAB)
+		{
+			//Switch to another fractal.
+		}
+		if (key == ESC)
+		{
+			mlx_destroy_window(f->mlx, f->win);
+			exit (0);
+		}
+	}
+	return (0);
+}
+
+int	initialize_graphic(t_fract *f)
+{
+	f->win = mlx_new_window(f->mlx, WIDTH, HEIGHT,
+			"Fract'ol | github.com/anselnettles");
+	mlx_string_put(f->mlx, f->win, 30, 20, NETTLE,
+		"Fract'ol |  \\ˈfrakt(a)l\\ | god dang fractals");
+	mlx_string_put(f->mlx, f->win, WIDTH - 440, HEIGHT - 65, NETTLE,
+		"Ansel Nettles | github.com/anselnettles");
+	mlx_string_put(f->mlx, f->win, WIDTH - 440, HEIGHT - 45, DAWN,
+		"\" C o d e - ' n - S w o r d \"");
+	return (0);
+}
