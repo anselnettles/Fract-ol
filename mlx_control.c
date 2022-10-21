719985fa4f835522fc14b35066e0dc1ac1d0e0ea
anselnettles <aviholai@student.hive.fi>
2022-10-21 18:01:54 +0300

    Mouse zoom controls.

--- mlx_control.c
+++ mlx_control.c
@@ -6,11 +6,40 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 18:21:54 by aviholai          #+#    #+#             */
-/*   Updated: 2022/10/21 14:59:02 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/10/21 17:57:53 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "the_uncontrolled_element_of_life.h"
+#include <stdio.h>
+
+int	mousepress(int key, int x, int y, void *param)
+{
+	t_fract *f;
+
+	f = (t_fract *)param;
+	if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT)
+		return (0);
+	if (key == ZOOM_IN)
+	{
+		printf("in. %f. ", f->increment);
+		f->increment -= (float)0.05;
+		//f->xside += (float)0.25;
+		//f->yside += (float)0.45;
+		//f->left -= (float)1.75;
+		//f->top -= (float)0.25;
+	}
+	if (key == ZOOM_OUT)
+	{
+		printf("out. %f. ", f->increment);
+		f->increment += (float)0.05;
+		//f->xside -= (float)0.25;
+		//f->yside -= (float)0.45;
+	}
+	mlx_clear_window(f->mlx, f->win);
+	mandelbrot(f);
+	return (1);
+}
 
 int	keypress(int key, void *param)
 {
