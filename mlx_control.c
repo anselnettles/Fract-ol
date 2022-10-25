9ed0031bb63ffc626633714cbbe3c43826aaa4cf
anselnettles <aviholai@student.hive.fi>
2022-10-25 18:34:55 +0300

    Cleaning. Should be finished now.

--- mlx_control.c
+++ mlx_control.c
@@ -6,7 +6,7 @@
 /*   By: aviholai <aviholai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 18:21:54 by aviholai          #+#    #+#             */
-/*   Updated: 2022/10/25 17:28:39 by aviholai         ###   ########.fr       */
+/*   Updated: 2022/10/25 18:30:35 by aviholai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	write_strings(t_fract *f)
 	mlx_string_put(f->mlx, f->win, 60, 65, DAWN,
 		"each of which is a reduced-size copy of the whole.");
 	mlx_string_put(f->mlx, f->win, 60, WINDOW - 160, GREY,
-		"| Move mouse to adjust the fractal | Scroll mouse to zoom. |");
+		"| Move mouse: Adjust fractal. | Scroll wheel: Zoom. | ESC: Quit. |");
 	mlx_string_put(f->mlx, f->win, WINDOW - 440, WINDOW - 65, NETTLE,
 		"Ansel Nettles | github.com/anselnettles");
 	mlx_string_put(f->mlx, f->win, WINDOW - 440, WINDOW - 45, DAWN,
@@ -31,7 +31,7 @@ void	write_strings(t_fract *f)
 
 int	mousepress(int key, int x, int y, void *param)
 {
-	t_fract *f;
+	t_fract	*f;
 
 	f = (t_fract *)param;
 	if (x < 0 || y < 0 || x > WINDOW || y > WINDOW)
@@ -62,25 +62,33 @@ int	keypress(int key, void *param)
 	return (0);
 }
 
-int motion(int x, int y, void *param)
+int	motion(int x, int y, void *param)
 {
-	t_fract *f;
+	t_fract	*f;
 
 	f = (t_fract *)param;
-
-	f->cursor_x = scale(x, -2, 2, WINDOW);
-	f->cursor_y = scale(y, -2, 2, WINDOW);
+	f->mouse_x = scale(x, -2, 2, WINDOW);
+	f->mouse_y = scale(y, -2, 2, WINDOW);
 	mlx_clear_window(f->mlx, f->win);
 	draw_fractal(f);
 	write_strings(f);
 	return (0);
 }
 
-int	initialize_graphic(t_fract *f)
+int	initialize(t_fract *f)
 {
 	f->mlx = mlx_init();
 	f->win = mlx_new_window(f->mlx, WINDOW, WINDOW,
 			"Fract'ol | github.com/anselnettles");
+	f->left = (float)-1.75;
+	f->top = (float)-0.25;
+	f->x_side = (float)0.25;
+	f->y_side = (float)0.45;
+	f->increment = 1;
+	if (f->request == TRICORN)
+		f->fractal_toggle = -2;
+	else
+		f->fractal_toggle = 2;
 	write_strings(f);
 	return (0);
 }
